2d51183c6dffc12a3d8464598aab706c364ccbee
hozenyth.andrade <hozenyth.andrade@gmail.com>
2024-09-18 11:21:30 -0300

    Teste Projeto DIO XP

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -11,21 +11,28 @@ namespace DesafioProjetoHospedagem.Models
         public Reserva(int diasReservados)
         {
             DiasReservados = diasReservados;
+
         }
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+
+            var capacidadeSuite = Suite.Capacidade;
+            var numeroHospedes = hospedes.Count();
+
+            if (capacidadeSuite >= numeroHospedes)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido             
                 // *IMPLEMENTE AQUI*
+                throw new Exception("Número de Hospedes excede a capacidade da Suíte");
             }
+
         }
 
         public void CadastrarSuite(Suite suite)
@@ -33,25 +40,26 @@ namespace DesafioProjetoHospedagem.Models
             Suite = suite;
         }
 
-        public int ObterQuantidadeHospedes()
+        public int ObterQuantidadeHospedes(List<Pessoa> hospedes)
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            return hospedes.Count();
         }
 
-        public decimal CalcularValorDiaria()
+        public decimal CalcularValorDiaria(int diasReservados, decimal valorDiaria)
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = diasReservados * valorDiaria;
+           
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (diasReservados >= 10)
             {
-                valor = 0;
+                valor = valor - (valor * (decimal)0.1);
             }
 
             return valor;
